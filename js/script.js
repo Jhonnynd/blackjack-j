15f9609da590d29bce11f922ce9c7add1bea6bcc
Jhonny <jhonny.nd@gmail.com>
2023-04-27 20:15:15 +0800

    changed console.logs for text in output and minimal css changes

--- js/script.js
+++ js/script.js
@@ -12,10 +12,10 @@ let start = false;
 
 checkGameStatus = () => { // check the game constantly, everytime it's called
     if (playerSum >= 21) {
-        console.log("you lost!");
+        output.innerHTML += `<p>you lost! </p>`;
         restartGame();
     } else if (dealerSum >= 21) {
-        console.log("Dealer lost!");
+        output.innerHTML += `<p>Dealer lost!</p>`;
         restartGame();
     }
 }
@@ -55,18 +55,19 @@ const dealerLogic = () => { // Logic of the dealer
     if (start === true) { //checks if the game restarted already
         let random = Math.floor(Math.random() * 2); // to ramdomize its move
         if (random === 0 || dealerSum < 17) {
-            console.log("Dealer chose to hit.")
+            output.innerHTML += `<p>Dealer chose to hit.</p>`;
             let dealerCard = grabACard();
-            console.log(`Dealer was handed a ${dealerCard[0]} of ${dealerCard[1]}`)
+            output.innerHTML += `<p>Dealer was handed a ${dealerCard[0]} of ${dealerCard[1]}</p>`;
             dealerSum += Number(dealerCard[0]);
             checkGameStatus();
         } else {
-            console.log("Dealer chose to stand.")
+            output.innerHTML += `<p>Dealer chose to stand.</p>`;
         }
     }
 }
 
 const restartGame = () => { // used to restart the game
+    output.innerHTML += `<p>Write "start" to restart the game!</p>`;
     start = false;
     playerSum = 0;
     dealerSum = 0;
@@ -74,26 +75,27 @@ const restartGame = () => { // used to restart the game
 }
 
 const startGame = () => { // function called when the game starts
-    console.log("the game starts!")
+    output.innerHTML += `<p>the game starts!</p>`
+    output.innerHTML = "";
     start = true;
     inputField.value = "";
     let playerCard1 = grabACard(); // first card the player gets
-    console.log(`you were handed a ${playerCard1[0]} of ${playerCard1[1]}`)
+    output.innerHTML += `<p>you were handed a ${playerCard1[0]} of ${playerCard1[1]}</p>`;
     playerSum += Number(playerCard1[0]);
     let playerCard2 = grabACard(); // second card the player gets
-    console.log(`you were handed a ${playerCard2[0]} of ${playerCard2[1]}`)
+    output.innerHTML += `<p>you were handed a ${playerCard2[0]} of ${playerCard2[1]}</p>`;
     playerSum += Number(playerCard2[0]);
     let dealerCard1 = grabACard(); // first card the dealer gets
-    console.log(`Dealer was handed a ${dealerCard1[0]} of ${dealerCard1[1]}`)
+    output.innerHTML += `<p>Dealer was handed a ${dealerCard1[0]} of ${dealerCard1[1]}</p>`;
     dealerSum += Number(dealerCard1[0]);
     let dealerCard2 = grabACard(); // second card the dealer gets
-    console.log(`Dealer was handed a ${dealerCard2[0]} of ${dealerCard2[1]}`)
+    output.innerHTML += `<p>Dealer was handed a ${dealerCard2[0]} of ${dealerCard2[1]}</p>`;
     dealerSum += Number(dealerCard2[0]);
     checkGameStatus();
 }
 
 const stand = () => { // Player choose to stand
-    console.log("You chose to stand")
+    output.innerHTML += `<p>You chose to stand </p>`;
     dealerLogic();
     checkGameStatus();
 }
@@ -104,11 +106,12 @@ submit.addEventListener("click", function () { // check input
     }
     else if (inputField.value.toLowerCase() === "hit") {
         if (!start) { // checks if the game started
-            console.log("you need to write start in order to start the game!")
+            alert("you need to write start in order to start the game!")
         } else {
             if (playerSum < 21) {
                 let playerCard = grabACard();
-                console.log(`you were handed a ${playerCard[0]} of ${playerCard[1]}`)
+                output.innerHTML += `<p>you chose to hit </p>`;
+                output.innerHTML += `<p>you were handed a ${playerCard[0]} of ${playerCard[1]} </p>`;
                 playerSum += Number(playerCard[0]);
                 inputField.value = "";
                 checkGameStatus();
@@ -117,12 +120,17 @@ submit.addEventListener("click", function () { // check input
         }
     }
     else if (inputField.value.toLowerCase() === "stand") {
-        stand();
+        if (!start) { // checks if the game started
+            alert("you need to write start in order to start the game!")
+        } else {
+            stand();
+        }
     }
      else {
         alert("You entered a wrong command.")
      }
 });
+
 
 buildDeck();
 shuffleDeck();
